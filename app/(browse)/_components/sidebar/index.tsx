7f083d572d1f7f70eeb7690e6c809df660b2ec73
idil <idil.iman1999@gmail.com>
2023-12-12 13:12:35 +0800

    feat: sidebar

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,25 @@
+import Recommended, { RecommendedSkeleton } from './recommended';
+import Toggle, { ToggleSkeleton } from './toggle';
+import { Wrapper } from './wrapper';
+import { getRecommended } from '@/lib/recommended-services';
+
+export default async function Sidebar() {
+  const recommended = await getRecommended();
+  return (
+    <Wrapper>
+      <Toggle />
+      <div className='space-y-4 pt-4 lg:pt-0'>
+        <Recommended data={recommended} />
+      </div>
+    </Wrapper>
+  );
+}
+
+export const SidebarSkeleton = () => {
+  return (
+    <aside className='fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50'>
+      <ToggleSkeleton />
+      <RecommendedSkeleton />
+    </aside>
+  );
+};
