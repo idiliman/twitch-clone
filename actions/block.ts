8dcf689e5916b086405e0699c21242bb4779b749
idil <idil.iman1999@gmail.com>
2023-12-15 15:38:39 +0800

    feat: browse page

--- actions/block.ts
+++ actions/block.ts
@@ -1,28 +1,35 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
+import { RoomServiceClient } from 'livekit-server-sdk';
 
 import { getSelf } from '@/lib/auth-service';
 import { blockUser, unblockUser } from '@/lib/block-service';
 
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
+
 export const onBlock = async (id: string) => {
   const self = await getSelf();
 
-  //   let blockedUser;
+  let blockedUser;
 
-  //   try {
-  //     blockedUser = await blockUser(id);
-  //   } catch {
-  //     // This means user is a guest
-  //   }
+  try {
+    blockedUser = await blockUser(id);
+  } catch {
+    // This means user is a guest
+  }
 
-  //   revalidatePath(`/u/${self.username}/community`);
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch {
+    // This means user is not in the room
+  }
 
-  const blockedUser = await blockUser(id);
-
-  revalidatePath('/');
-
-  if (blockedUser) revalidatePath(`/${blockedUser.blocked.username}`);
+  revalidatePath(`/u/${self.username}/community`);
 
   return blockedUser;
 };
@@ -31,9 +38,6 @@ export const onUnblock = async (id: string) => {
   const self = await getSelf();
   const unblockedUser = await unblockUser(id);
 
-  //   revalidatePath(`/u/${self.username}/community`);
-
-  if (unblockedUser) revalidatePath(`/${unblockedUser.blocked.username}`);
-
+  revalidatePath(`/u/${self.username}/community`);
   return unblockedUser;
 };
--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -2,6 +2,8 @@ import { isFollowingUser } from '@/lib/follow-service';
 import { getUserByUsername } from '@/lib/user-service';
 import { notFound } from 'next/navigation';
 import Actions from './_components/actions';
+import { isBlockedByUser } from '@/lib/block-service';
+import StreamPlayer from '@/components/stream-player';
 
 interface UserPageProps {
   params: {
@@ -12,15 +14,16 @@ interface UserPageProps {
 export default async function UserPage({ params }: UserPageProps) {
   const user = await getUserByUsername(params.username);
 
-  if (!user) notFound();
+  if (!user || !user.stream) {
+    notFound();
+  }
 
   const isFollowing = await isFollowingUser(user.id);
+  const isBlocked = await isBlockedByUser(user.id);
 
-  return (
-    <div>
-      <p>User: {user?.username}</p>
-      <p>is following: {`${isFollowing}`}</p>
-      <Actions isFollowing={isFollowing} userId={user.id} />
-    </div>
-  );
+  if (isBlocked) {
+    notFound();
+  }
+
+  return <StreamPlayer user={user} stream={user.stream} isFollowing={isFollowing} />;
 }
