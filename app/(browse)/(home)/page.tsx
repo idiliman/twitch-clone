edcbff3f62cba699feb7b74ba7874ad6198dc1e4
idil <idil.iman1999@gmail.com>
2023-12-15 15:44:18 +0800

    feat: home page feeds

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,5 +1,13 @@
-import Image from 'next/image';
+import { Suspense } from 'react';
 
-export default function Home() {
-  return <h1>Home</h1>;
+import { Results, ResultsSkeleton } from './_components/results';
+
+export default function Page() {
+  return (
+    <div className='h-full p-8 max-w-screen-2xl mx-auto'>
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
+    </div>
+  );
 }
